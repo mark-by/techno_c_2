bbc6bf876c36fb85fd2c9c246dbca92bc06918ee
Mark Bykhovets <mb@hrsmnt.ru>
2020-03-23 20:34:13 +0300

    подключил gtest

--- test/count_symbols.cpp
+++ test/count_symbols.cpp
@@ -1,4 +1,4 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include <cstdio>
 #include <sys/mman.h>
 #include <sys/wait.h>
@@ -8,14 +8,14 @@ extern "C" {
 }
 
 TEST(CountSymbols, StartProcesses) {
-int amount = 10;
-int *shareable_array = static_cast<int *>(mmap(NULL, sizeof(int) * amount,
+    int amount = 10;
+    int *shareable_array = static_cast<int *>(mmap(NULL, sizeof(int) * amount,
                                                PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
-int id;
-start_processes(10, &id);
-shareable_array[id] = 1;
-while(wait(nullptr) > 0);
-for (int i = 0; i < amount; i++) {
-    ASSERT_EQ(1, shareable_array[i]);
-}
+    int id;
+    start_processes(amount, &id);
+    shareable_array[id] = 1;
+    while(wait(nullptr) > 0);
+    for (int i = 0; i < amount; i++) {
+        ASSERT_EQ(1, shareable_array[i]);
+    }
 }
